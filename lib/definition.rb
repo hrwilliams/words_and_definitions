4741cd8487ec8158c40e6daf5e469cfc2ab98c52
Halle Williams <williams.hr@gmail.com>
2016-07-08 08:59:39 -0700

    adds method to return definition string

--- lib/definition.rb
+++ lib/definition.rb
@@ -2,4 +2,8 @@ class Definition
   define_method(:initialize) do |definition_string|
     @definition_string = definition_string
     end
+
+  define_method(:definition_string) do
+    @definition_string
   end
+end
