c82cbf8272be6021211f76c8d152db78bd3e8172
Halle Williams <williams.hr@gmail.com>
2016-07-08 12:01:31 -0700

    edit word_string spec to properly test for word_string method, add passing method for word_string

--- lib/word.rb
+++ lib/word.rb
@@ -1,2 +1,10 @@
 class Word
+  define_method(:initialize) do |word_string|
+    @word_string = word_string
+  end
+
+  define_method(:word_string) do
+    @word_string
+  end
+
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -6,7 +6,7 @@ describe(Word) do
   describe('#word_string') do
     it("returns the word string for the word") do
       test_word = Word.new("trill")
-      expect(test_word.name()).to(eq("trill"))
+      expect(test_word.word_string()).to(eq("trill"))
     end
   end
 end
